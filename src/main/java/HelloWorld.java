78cb859ba64b46fd10874e3bd8db641875123dba
balu1391 <94595511+balu1391@users.noreply.github.com>
2022-02-07 13:29:48 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/my-page", (req, res) -> "This is my first java application");
+		get("/my-page", (req, res) -> "This is my first java application page");
 	}
 }
