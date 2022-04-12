4505ea9401d12c8351a5904502a4c0a70b59f8c4
bala <balu391balu@gmail.com>
2022-04-12 15:03:10 +0530

    added

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+import spark.servlet.SparkApplication;
+
+import static spark.Spark.get;
+
+public class HelloWorld implements SparkApplication {
+	public static void main(String[] args) {
+		new HelloWorld().init();
+	}
+
+	@Override
+	public void init() {
+		get("/my-page", (req, res) -> "This is my first java application page2");
+	}
+}
